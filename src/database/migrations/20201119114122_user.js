88f6e8276f8b2c99312e40d7a7c3b4d39026047f
Raphael Leivas <raphaelhenrique2013@outlook.com>
2020-11-20 14:15:58 -0300

    Criação das migrations, DBeaver funcional

--- src/database/migrations/20201119114122_user.js
+++ src/database/migrations/20201119114122_user.js
@@ -1,8 +1,10 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('user', function(table){
+      table.string('username').primary().notNullable();
+      table.string('password').primary().notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('user');
 };
---
+++ src/database/migrations/20201119213706_personagem.js
@@ -0,0 +1,15 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('personagem', function(table){
+        table.string('nome').primary().notNullable();
+        table.foreign('nome').references('username').inTable('user').onDelete("cascade");
+        table.string('genero').primary().notNullable();
+        table.string('classe').primary().notNullable();
+        table.string('cla').primary().notNullable();
+        table.integer("idade").primary().notNullable();
+    });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTable('personagem');
+  };
+  
